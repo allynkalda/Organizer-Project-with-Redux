07319b7f0fdd7d401627a224a6cdd88ab892537d
Allyn Alda <Teddy@om.home>
2019-06-17 14:35:49 +0200

    implemented redux and firebase connection

---
+++ src/config/fbconfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCFH5ixUGXeV_dFYMyh_GE3_C2SeqHj8hM",
+    authDomain: "organizer-project-with-redux.firebaseapp.com",
+    databaseURL: "https://organizer-project-with-redux.firebaseio.com",
+    projectId: "organizer-project-with-redux",
+    storageBucket: "organizer-project-with-redux.appspot.com",
+    messagingSenderId: "496211745636",
+    appId: "1:496211745636:web:22c486a6c5f858f4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase
