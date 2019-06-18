eda50459c6245539bf269d58b573feea89feab4c
Allyn Alda <Teddy@Om.local>
2019-06-18 15:00:42 +0200

    base done

---
+++ dist/precache-manifest.1b068ddc1c13bbd7e92179b7030a16a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b56da16dd98f6b0d7628bd6c8a65734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4c1176de5e26535ed5",
+    "url": "/static/css/main.3a1cd80e.chunk.css"
+  },
+  {
+    "revision": "3a76955b728bc1016005",
+    "url": "/static/js/2.861e6c7a.chunk.js"
+  },
+  {
+    "revision": "4e4c1176de5e26535ed5",
+    "url": "/static/js/main.f909f935.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
