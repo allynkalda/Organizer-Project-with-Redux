570d15f730e68e60bd42835674b708a64ef0ba69
Allyn Alda <Teddy@om.home>
2019-06-26 22:33:38 +0200

    showing only todo's of user

--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Redirect } from 'react-router-dom'
 
 class TodoList extends Component {
-    
     render() {
+        const { todo, auth } = this.props;
+        console.log(todo)
 
-        console.log(this.props)
+        if (!auth.uid) return <Redirect to='/signin' />
+
+        if (!todo) return <p>You have no items in your to-do list.</p>
+        
         return (
             <div>
                 <h5 className="grey-text text-darken-3">My To-Do List</h5>
                 <div className="container">
-                <Link to="/newtodo"><button className="btn pink lighten-1 z-depth-0">Create a To-Do</button></Link>
+                    {
+                        !todo ? "Loading" : todo.map( todo => <p>{todo.title}</p>)
+                    }
+                </div>
+                <div className="container">
+                    <Link to="/newtodo"><button className="btn pink lighten-1 z-depth-0">Create a To-Do</button></Link>
                 </div>
             </div>
         )
@@ -19,10 +31,20 @@ class TodoList extends Component {
 }
 
 const mapStateToProps = (state) => {
-        console.log(state)
+    const todos = state.firestore.ordered.todo;
     return {
-        todos: state.todos
+        todo: todos,
+        auth: state.firebase.auth
     }
 }
 
-export default connect(mapStateToProps)(TodoList);
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect((props) => {
+        return [
+        { collection: 'todo',
+        where: [
+           ['authorId', '==', props.auth.uid]
+        ] }
+    ]})
+)(TodoList);
