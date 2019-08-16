ce88acbe0b564f1b99905378da55018f45fc5490
Allyn Alda <Teddy@om.home>
2019-08-16 12:40:08 +0200

    fixed cards for comments

--- src/components/project/CommentsList.js
+++ src/components/project/CommentsList.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import { firestoreConnect } from "react-redux-firebase"
-import { connect } from 'react-redux'
-import { compose } from 'redux'
+import React from 'react';
+import { firestoreConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import moment from 'moment';
 
 const CommentsList = ({ comments }) => {
     return (
+
         <div>
             { !comments ? "Loading..." : comments.map(elem => {
                 return (
-                    <div key={elem.id}>
-                        <p>{elem.comment}</p>
-                        <p>Posted by {elem.authorFirstName}</p>
+                    <div class="row">
+                        <div class="col s12 m12">
+                            <div class="card light-grey darken-1">
+                                <div class="card-content dark-grey-text">
+                                    <div key={elem.id}>
+                                    <h5>{elem.comment}</h5>
+                                    </div>
+                                    </div>
+                                <div className="card-action grey lighten-4 grey-text">
+                                <p>Posted by {elem.authorFirstName}</p>
+                                <p>{ elem.createdAt ? 
+                                    moment(elem.createdAt.toDate().toString()).calendar() :
+                                    null }</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )
             }) }
